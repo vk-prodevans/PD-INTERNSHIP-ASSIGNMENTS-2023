fe2523cc54c2bc4b985bc129b557a92a7b372746
vk-prodevans <jalagam.kumar@prodevans.com>
2023-09-15 16:06:07 +0530

    Message

--- index.js
+++ index.js
@@ -20,8 +20,9 @@
 //             console.error("There was a problem fetching the data:", error);
 //         });
 // });
-const url='https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx%2C57dN52uHvrHOxijzpIgu3E%2C1vCWHaC5f2uS3yhpwWbIA6'
-const token = 'BQDj6Y3Zuyb9D6uEXfbkKXMZBVsBNCh6azY7tXCZjumyXQ7PnkrG3aI9RO_jD3zUZrXl3kJ2lEWfYTqE7gCY44-pO598wRom5ZC9w8WM4v_UdVo5FIP3wkElUfHpNARo52MkzD4rTH2XSwWsL806NUJvlde00mClAU6B9C-EiXnQz0OD4f52ad1DOOEXiYAwcKNxTLx9g4VzA5976iw-BuNTPK3-MzMK3g9vwED9WOwFMmctJsrLJ3mku-ux4rDsPCeS9nivUHWrpgW2cggzZxBH';
+const url = 'https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx,4YRxDV8wJFPHPTeXepOstw,0oOet2f43PA68X5RxKobEy,4zCH9qm4R2DADamUHMCa6O,7qjJw7ZM2ekDSahLXPjIlN,4PULA4EFzYTrxYvOVlwpiQ,5sSzCxHtgL82pYDvx2QyEU,5sSzCxHtgL82pYDvx2QyEU,5wJ1H6ud777odtZl5gG507';
+
+const token = 'BQC4pNyNXqqi2Q0jLcz5Vq-fDqVYuKF2VViIdL4M-Gsnh023yxtF28GpTGAR_H3h90z22d8wvPX1hr1PmLkzBiS0JNTX2Sl61OOZK8sZAOTJuTfb8jlv1eSnFANwjlQaoY0IXA8ypbE5vDF3fd_iGNvZElnqrGLdpYh7C_B1kA8pzWe-3CDzYlAsgS2FSpRxMb45zgPJw1fziVfYy7bSVeDsYmIEVvlzkG6keD4zDgDdw5eHVE9aGKnLdy0eThlKsHMhLK1TRUSd4UMDWOfOxGvG';
 const request = new Request(
     url,{
         headers:{
@@ -40,13 +41,18 @@ const request = new Request(
             const dataContainer = document.getElementById("assignment");
 
             data.artists.forEach(artist => {
-                const artistlist= document.createElement("h1");
-                artistlist.textContent = `Artist Name: ${artist.name}`;
-                dataContainer.appendChild(artistlist);
+                const artistname= document.createElement("h1");
+                artistname.textContent = `Artist Name: ${artist.name} [Artist Genres:${artist.genres}]`;
+                dataContainer.appendChild(artistname);
+                
             });
+            // data.artists.forEach(artist => {const artistgenres = document.createElement("h2");
+            //     artistgenres.textContent = `Artist Genres:${artist.genres}`;
+            //      dataContainer.appendChild(artistgenres)});
+
 
             
-            dataContainer.appendChild(artistList);
+            
         } catch (error) {
             console.error("Error fetching data:", error);
         }
